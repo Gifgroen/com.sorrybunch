34000f3a87619fdb3d3fd170e4e71ec10d84fa1c
Karsten Westra <karsten@peperzaken.nl>
2013-04-03 16:12:21 +0200

    Added live DB settings.

--- settings.py
+++ settings.py
@@ -153,15 +153,15 @@ STATICFILES_FINDERS = (
 DATABASES = {
     "default": {
         # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
-        "ENGINE": "django.db.backends.",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
         # DB name or path to database file if using sqlite3.
-        "NAME": "",
+        "NAME": "sorrybunchcom",
         # Not used with sqlite3.
-        "USER": "",
+        "USER": "sorrybunch",
         # Not used with sqlite3.
-        "PASSWORD": "",
+        "PASSWORD": "s0RRyBun(h",
         # Set to empty string for localhost. Not used with sqlite3.
-        "HOST": "",
+        "HOST": "localhost",
         # Set to empty string for default. Not used with sqlite3.
         "PORT": "",
     }
