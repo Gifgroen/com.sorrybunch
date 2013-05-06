a6d7e40669606697c1dbbd34907d6b1184ca1e1a
Karsten Westra <karstenwestra@gmail.com>
2013-05-06 19:56:44 +0200

    Added app for photos page. Changed paths to images in photo navigator.

--- urls.py
+++ urls.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 
 from mezzanine.core.views import direct_to_template
 
-from home import views
+import home.views
+import photos.views
 
 admin.autodiscover()
 
@@ -27,7 +28,8 @@ urlpatterns = patterns("",
     # commented out like the others, so it's the default. You only need
     # one homepage pattern, so if you use a different one, comment this
     # one out.
-	url("^$",  views.index),
+	url("^$",  home.views.index),
+    url("^photos$", photos.views.index),
 
     url("^$", direct_to_template, {"template": "index.html"}, name="home"),
 
