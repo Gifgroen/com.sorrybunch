f65bce4e27bfe6b1e1cf58528d0bfa9c081ee8b3
Karsten Westra <karstenwestra@gmail.com>
2013-05-15 10:35:19 +0200

    Removed the old project. Mezzanine is to much for this.

--- home/views.py
+++
@@ -1,8 +0,0 @@
-# Create your views here.
-from django.shortcuts import render_to_response
-
-def index(request):
-    return render_to_response('home.html', '', {})
-
-def page_not_found(request):
-    return render_to_response('404.html', '', {})
--- newsandgigs/views.py
+++
@@ -1,8 +0,0 @@
-# Create your views here.
-from django.shortcuts import render_to_response
-
-def news(request):
-    return render_to_response('news.html', '', {})
-
-def gigs(request):
-    return render_to_response('gigs.html', '', {})
